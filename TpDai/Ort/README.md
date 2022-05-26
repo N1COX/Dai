0ed7d16846446bb6694a37f657943aba9b97357e
N1COX <105858377+N1COX@users.noreply.github.com>
2022-05-26 09:07:30 -0300

    Add files via upload

---
+++ TpDai/Ort/README.md
@@ -0,0 +1,2 @@
+# Ort
+Acá se guardan todos los tps de quinto año
---
+++ TpDai/Ort/Tp1_Node/SRC/Matematica.js
@@ -0,0 +1,15 @@
+// Ejercicio 2
+
+const tres=3;
+export function DividirPor2(Dividir){
+    return Dividir/2;
+}
+export function Suma(Sumar){
+    return Sumar+1;
+}
+export let Multiplicacion = (Multiplicar)=>{
+return Multiplicar*2;
+}
+export let MultiplicarPor3= (Multiplicar)=>{
+    return Multiplicar*3;
+}
