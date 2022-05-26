cc13297fffac239a29c630330488e68d4fa37c00
N1COX <105858377+N1COX@users.noreply.github.com>
2022-05-26 09:42:06 -0300

    Delete Tp1_Node directory

--- Tp1_Node/Ejercicio5.js
+++
@@ -1,8 +0,0 @@
-let objeto = parsearUrl('http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo')
-console.log(objeto)
-
-params: {
-    "host": 
-    "pathname": 
-    "parametros": 
-  }
--- Tp1_Node/index.js
+++
@@ -1,5 +0,0 @@
-   
-
-
-let Tito = new alumno("Tito", 12345432);
-let Rabih = new alumno("Rabih", 46437748);
